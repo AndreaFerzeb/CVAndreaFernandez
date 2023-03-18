b12989972739bf7319bafd94171e01ae3ba49ae1
unknown <amferzeb@gmail.com>
2023-03-18 20:21:07 -0300

    first commit

---
+++ js/funciones.js
@@ -0,0 +1,9 @@
+//Alerta para el usuario/
+function alertaimagen() { 
+    alert('Usted hizo click en Redes Sociales. Presione enter para continuar');
+
+}
+
+function onclick (){
+    document.getElementById ('correo').innerHTML="¡Gracias!";
+}
